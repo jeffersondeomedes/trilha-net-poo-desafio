b8889d932cb8a217dd3f9d67fa90f51b40dba752
jeffersondeomedes <jeffersondeomedes@hotmail.com>
2024-01-05 22:20:51 -0300

    main

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "876633", modelo: "C2232", imei: "1110010101110", memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "5432323", modelo: "8 PLUS", imei: "233332323232", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("LinkedIn");
